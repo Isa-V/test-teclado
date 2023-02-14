6f0f0553f45993f0ede77f0e28f13a3c8346e773
Isa-V <66229178+Isa-V@users.noreply.github.com>
2023-02-14 13:20:17 -0300

    cambio del codigo js

--- script.js
+++ script.js
@@ -1,7 +1,73 @@
 
 const text = document.querySelector("#titulo");
 
-// resize event listener to detect change in screen height
-window.addEventListener("resize", (e) => {
-    text.innerHTML = "Virtual keyboard detected!!!";
+function onKeyboardOnOff(isOpen) {
+    // Write down your handling code
+    if (isOpen) {
+        // keyboard is open
+        text.innerHTML = "Virtual keyboard detected!!!";
+    } else {
+        // keyboard is closed
+        text.innerHTML = "Ya no hay teclado";
+    }
+}
+
+var originalPotion = false;
+$(document).ready(function(){
+    if (originalPotion === false) originalPotion = $(window).width() + $(window).height();
+});
+
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+ *
+ * @returns {String}
+ */
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "winphone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "ios";
+    }
+
+    return "";
+}
+
+function applyAfterResize() {
+
+    if (getMobileOperatingSystem() != 'ios') {
+        if (originalPotion !== false) {
+            var wasWithKeyboard = $('body').hasClass('view-withKeyboard');
+            var nowWithKeyboard = false;
+
+                var diff = Math.abs(originalPotion - ($(window).width() + $(window).height()));
+                if (diff > 100) nowWithKeyboard = true;
+
+            $('body').toggleClass('view-withKeyboard', nowWithKeyboard);
+            if (wasWithKeyboard != nowWithKeyboard) {
+                onKeyboardOnOff(nowWithKeyboard);
+            }
+        }
+    }
+}
+
+$(document).on('focus blur', 'select, textarea, input[type=text], input[type=date], input[type=password], input[type=email], input[type=number]', function(e){
+    var $obj = $(this);
+    var nowWithKeyboard = (e.type == 'focusin');
+    $('body').toggleClass('view-withKeyboard', nowWithKeyboard);
+    onKeyboardOnOff(nowWithKeyboard);
+});
+
+$(window).on('resize orientationchange', function(){
+    applyAfterResize();
 });
